a30bf1d2990340127b40ad16b74888f6915e9f55
evolutionoftheuniverse <67762964+evolutionoftheuniverse@users.noreply.github.com>
2020-11-02 05:58:28 +0300

    PERCENTAGE

--- peksimet/finders.py
+++ peksimet/finders.py
@@ -1,1 +1,10 @@
+import logging
+import re
+try:
+    import regex
+except ImportError:
+    regex = re
 
+PERCENTAGE = regex.compile(r'''(?:\s|\||\s\(|:\s|}})\s?(?:-?|\+?|<?|>?|~?|±?|–?|∓?|≈?|≤?|≥?)\s?
+(([0-9]+)(\.?|,?)([0-9]*)( ?\%| ?‰)|(\% ?|(?:Y|y)üzde ?|‰ ?|(?:B|b)inde ?)([0-9]+)(\.?|,?)([0-9]*))
+(?:\s|\||\)|:|,|'|’|}}|\s?-\s?)'''.replace('\n', ''))
