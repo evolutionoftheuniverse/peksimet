3fff57cbd5d3515a49512aaaff60f83314622cf0
evolutionoftheuniverse <67762964+evolutionoftheuniverse@users.noreply.github.com>
2020-11-07 02:38:45 +0300

    TEMPLATE_REF and SECTION

--- peksimet/filters.py
+++ peksimet/filters.py
@@ -22,6 +22,24 @@ except ImportError:
 LOGGER = logging.getLogger(__name__)
 
 TAG_REF = regex.compile(r'(<ref[^>]*>|<\/ref>)')
+TEMPLATE_REF = regex.compile(r'''({{(?:(?:Web|(?:İ|i)nternet|ArXiv|Instagram|Tweet|Twitter|
+(?:Bilimsel ?)rapor|Konferans|E-posta|bioRxiv|Citeseerx|Haber(?: ?grubu)?|Görüşme|Röportaj|
+Mülakat|Tabela|Podcast|Konuşma|Harita|Kitap|(?:Akademik )?dergi|Tez|Bölüm|Dizi|Program|
+Video(?: oyunu)?|Ortam|Medya(?: notları)?|Ses|Ansiklopedi|Sözlük|DNB|Yasa|Banglapedia|
+Yahudi Ansiklopedisi|Basın ?(?:açıklaması|bildirisi|bülteni|duyurusu|ilanı)? ?kaynağı)|
+Cite ?(?:web|arxiv|bioRxiv|citeseerx|twitter|tweet|report|conference|newsgroup|interview|
+(?:album-|DVD-|AV media )notes|thesis|doi|pmid|podcast|map|speech|book|video game|
+press ?(?:release)?|techreport|journal|paper|document|science|episode|show|video|media|
+audio|encyclopedia|dictionary|SEP|NIE|Americana|Katolik Ansiklopedi|Catholic Encyclopedia|
+EB1911|EB9|AmCyc|NSRW|DCBL|ODNB|Jewish Encyclopedia|AV media)
+Include-USGov|Wikicite|Citation|Harvnb|Sfn|Kaynak|Harvard citation no brackets|Kdş|NRISref|
+Tweet|Scientific American Frontiers|Doi(?:-inline)|Bibcode|Kaynak Kitap|Video oyunu belirt|
+PMID|YouTube|IEP|CathEncy|Oxford Dictionary of Byzantium|ODB|TES|DSB|PLRE|PMBZ|DCBL|
+Encyclopaedia of Islam, New Edition|Prosopography of the Later Roman Empire|Who's Who|
+Prosopographi(?: der mittelbyzantinischen Zeit|sches Lexikon der Palaiologenzeit)|
+MSW3 Diprotodontia).*\r?\n?(?:\|.*\r?\n?)+}})'''.replace('\n', ''), regex.I)
+SECTION = regex.compile(r'(={1,6})([^\n]+?)\1[ \t]*(?:\n|\Z)')
+
 
 def filter_ref(text, expr_start, expr_end):
     """Expression check between ref tags."""
